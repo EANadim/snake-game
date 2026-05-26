05c9b1c79d4038608f5633b1e9f9307a13831ace
Ehtesham Nadim <ehtesham.nadim@riiico.com>
2026-05-26 16:55:10 +0200

    setting up the frontend for single player game

---
+++ frontend/src/App.vue
@@ -0,0 +1,19 @@
+<script setup>
+import SnakeGame from './components/SnakeGame.vue'
+</script>
+
+<template>
+  <main class="app">
+    <SnakeGame />
+  </main>
+</template>
+
+<style scoped>
+.app {
+  min-height: 100vh;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  padding: 16px;
+}
+</style>
